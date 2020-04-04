fc9ed492c96dcf106d906e8a03c5d92d6e1876ff
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-04-04 06:26:53 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -31,11 +31,7 @@ class Book
     @genre
   end
 
-  def turn_page=(turn_page)
-    @turn_page = turn_page
-  end
-
   def turn_page
-    @turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
 end
